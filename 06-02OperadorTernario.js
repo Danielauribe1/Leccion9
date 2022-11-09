d62bfedb38449e2611f6923f9911bb59a015cda5
DUM <114031187+Danielauribe1@users.noreply.github.com>
2022-11-09 17:28:42 -0500

    Segundo avance segunda semana de noviembre

---
+++ 06-02OperadorTernario.js
@@ -0,0 +1,8 @@
+//Operador ternario
+
+let resultado = (1>2) ? "verdadero" : "falso";
+console.log(resultado);
+
+let numero = 110;
+resultado = ( numero % 2 == 0 ) ? "Número par" : "Número impar"; 
+console.log( resultado );
